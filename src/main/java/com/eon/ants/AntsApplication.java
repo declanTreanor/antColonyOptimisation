404edad214a44bd31c4800a4802d4fc90ff81c67
Treanor <d26553@eon.com>
2023-01-27 14:57:49 +0100

    removed comments

--- src/main/java/com/eon/ants/AntsApplication.java
+++ src/main/java/com/eon/ants/AntsApplication.java
@@ -15,54 +15,6 @@ public class AntsApplication {
 
 	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(AntsApplication.class, args);
-//		//		new AntsApplication().findShortestPath();
-//		ApplicationContext ctx = new AnnotationConfigApplicationContext(ACOConfig.class);
-//		PheremoneManager pheremoneManager = null;
-//		Ant ant;
-//		int amountOfAnts = getNumberOfAnts(ctx.getBean(ACOConfig.class).nodeNames().length);
-//		for (int i = 0; i < amountOfAnts; i++) {
-//
-//			if (i % 5 == 0) {
-//				if (pheremoneManager == null)
-//					pheremoneManager = (PheremoneManager) ctx.getBean("PheremoneManager");
-//				else {
-//					pheremoneManager.evaporate(0.9D);
-//				}
-//
-//			}
-//
-//
-//			doRunAround(ctx, pheremoneManager, i);
-
-		}
-
-
-//		System.out.println(pheremoneManager.getAllChanges().toString());
-//		System.out.println("\nthe following comprises the best attempts found, though not looked-for (so, therefore not definitive), starting at various attractions:\n");
-//		System.out.println(pheremoneManager.getAll6RoutesFromDifferentStarts());
-//		System.out.println("\n but (one of) the absolute shortest path(s) is: "+pheremoneManager.getShortestPath());
-
 	}
-
-//	private static void doRunAround(ApplicationContext ctx, PheremoneManager pheremoneManager, int i) {
-//		Ant ant;
-//		ant = (Ant) ctx.getBean("Ant");
-//		ant.startOnRandomNode();
-//
-//		while (ant.getPathTaken().size() < ant.getNodeNames().length) {
-//
-//			ant.moveToNextNode();
-//
-//		}
-//		pheremoneManager.saveRoute(ant, i);
-//	}
-//
-//	private static int getNumberOfAnts(int amountOfNodes) {
-//		int amount = 1;
-//		for(int i=1; i<amountOfNodes; i++)
-//			amount *= i;
-//
-//		return amount;
-//	}
-
+}
 
